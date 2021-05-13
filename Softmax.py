ad2bd67d763eb2251542719eee3b667f1fdfe6b0
nowwhy <nowwhy@naver.com>
2021-05-13 15:21:41 +0900

    softmax

---
+++ Softmax.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def softmax(x):
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T
+
+    x = x -np.max(x) #오버플로 대책
+    return np.exp(x) / np.sum(np.exp(x))
